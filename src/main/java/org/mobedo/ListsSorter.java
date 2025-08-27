f1dabb73cbfcac6040abd296fe09474324d373c8
Jonathan Bedaux <jonathan.bedaux@external.idemia.com>
2025-08-27 12:01:27 +0200

    added sorter logic and tests

--- src/main/java/org/mobedo/ListsSorter.java
+++ src/main/java/org/mobedo/ListsSorter.java
@@ -1,12 +1,20 @@
 package org.mobedo;
 
 import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ListsSorter
 {
     public AbstractMap.SimpleEntry<List<Integer>, List<Integer>> sort(AbstractMap.SimpleEntry<List<Integer>,List<Integer>> input)
     {
-        return null;
+        List<Integer> sortedKeyList = new ArrayList<>(input.getKey());
+        List<Integer> sortedValueList = new ArrayList<>(input.getValue());
+
+        Collections.sort(sortedKeyList);
+        Collections.sort(sortedValueList);
+
+        return new AbstractMap.SimpleEntry<>(sortedKeyList, sortedValueList);
     }
 }
--- src/test/java/org/mobedo/SorterTest.java
+++ src/test/java/org/mobedo/SorterTest.java
@@ -1,30 +1,41 @@
 package org.mobedo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SorterTest
 {
-
-    @Test
-    void Sorter_emptyList_returnsEmptyLists()
+    @ParameterizedTest
+    @MethodSource("InputSorterTest")
+    void Sorter_multiEntryList_returnsCorrectSortedLists(AbstractMap.SimpleEntry<List<Integer>,List<Integer>> input, AbstractMap.SimpleEntry<List<Integer>,List<Integer>> expected)
     {
         //Arrange
-        AbstractMap.SimpleEntry<List<Integer>,List<Integer>> input = new AbstractMap.SimpleEntry<List<Integer>,List<Integer>>(
-                new ArrayList<>(),new ArrayList<>()
-        );
         ListsSorter sut = new ListsSorter();
 
         //Act
         AbstractMap.SimpleEntry<List<Integer>, List<Integer>> result = sut.sort(input);
 
         //Assert
-        assertTrue(result.getKey().isEmpty());
-        assertTrue(result.getValue().isEmpty());
+        assertEquals(expected.getKey(), result.getKey());
+        assertEquals(expected.getValue(), result.getValue());
 
     }
+
+    private static Stream<Arguments> InputSorterTest() {
+        return Stream.of(
+                Arguments.of( new AbstractMap.SimpleEntry<>(List.of(),  List.of()), new AbstractMap.SimpleEntry<>(List.of(), List.of())),
+                Arguments.of( new AbstractMap.SimpleEntry<>(List.of(1),  List.of(2)), new AbstractMap.SimpleEntry<>(List.of(1), List.of(2))),
+                Arguments.of( new AbstractMap.SimpleEntry<>(List.of(1, 2),  List.of(3, 4)), new AbstractMap.SimpleEntry<>(List.of(1,2), List.of(3,4))),
+                Arguments.of( new AbstractMap.SimpleEntry<>(List.of(2, 1),  List.of(4, 3)), new AbstractMap.SimpleEntry<>(List.of(1,2), List.of(3,4))));
+    }
+
 }
